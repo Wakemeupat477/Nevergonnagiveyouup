1b915915f185fb08af264bd4370d7754e2e837da
efafaf <fasfasf>
2021-02-27 16:32:30 +0300

    kavo

---
+++ MainSattings.py
@@ -0,0 +1,84 @@
+import math
+import pygame
+
+# game settings
+WIDTH = 1920
+HEIGHT = 1000
+HALF_WIDTH = WIDTH // 2
+HALF_HEIGHT = HEIGHT // 2
+FPS = 60
+TILE = 100
+
+# ray casting settings
+FOV = math.pi / 3
+HALF_FOV = FOV / 2
+NUM_RAYS = 120
+MAX_DEPTH = 800
+DELTA_ANGLE = FOV / NUM_RAYS
+DIST = NUM_RAYS / (2 * math.tan(HALF_FOV))
+PROJ_COEFF = 3 * DIST * TILE
+SCALE = WIDTH // NUM_RAYS
+
+# player settings
+player_pos = (HALF_WIDTH, HALF_HEIGHT)
+player_angle = 0
+player_speed = 2
+
+# colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (220, 0, 0)
+GREEN = (0, 0, 0)
+BLUE = (0, 0, 255)
+DARKGRAY = (40, 40, 40)
+PURPLE = (120, 0, 120)
+
+#Map
+text_map = [
+    'WWWWWWWWWWWW',
+    'W.......W..W',
+    'W......W...W',
+    'W..........W',
+    'W..W.......W',
+    'W..........W',
+    'W.....W....W',
+    'WWWWWWWWWWWW'
+]
+
+world_map = set()
+for j, row in enumerate(text_map):
+    for i, char in enumerate(row):
+        if char == 'W':
+            world_map.add((i * TILE, j * TILE))
+
+
+
+class Player:
+    def __init__(self):
+        self.x, self.y = player_pos
+        self.angle = player_angle
+
+    @property
+    def pos(self):
+        return (self.x, self.y)
+
+    def movement(self):
+        sin_a = math.sin(self.angle)
+        cos_a = math.cos(self.angle)
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            self.x += player_speed * cos_a
+            self.y += player_speed * sin_a
+        if keys[pygame.K_s]:
+            self.x += -player_speed * cos_a
+            self.y += -player_speed * sin_a
+        if keys[pygame.K_a]:
+            self.x += player_speed * sin_a
+            self.y += -player_speed * cos_a
+        if keys[pygame.K_d]:
+            self.x += -player_speed * sin_a
+            self.y += player_speed * cos_a
+        if keys[pygame.K_LEFT]:
+            self.angle -= 0.02
+        if keys[pygame.K_RIGHT]:
+            self.angle += 0.02
